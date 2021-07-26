325fc4d6122d1109b438431c3106164f2b398dd5
Teofil-Ioan Vartolomei <teo.vty@gmail.com>
2021-07-26 14:37:46 +0200

    Configure project for Heroku

--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -61,7 +61,7 @@ export default function Jobs(props: Props) {
   const toggle = () => setModal(!modal);
 
   async function clickHandler() {
-    const response = await fetch(`/api/jobs/create`, {
+    const response = await fetch(`http://localhost:3000/api/jobs/create`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -425,7 +425,7 @@ export default function Jobs(props: Props) {
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const response = await fetch(`${process.env.BASE_URL}/jobs`, {
+  const response = await fetch(`http://localhost:3000/jobs`, {
     method: 'GET',
     headers: {
       cookie: context.req.headers.cookie || '',
